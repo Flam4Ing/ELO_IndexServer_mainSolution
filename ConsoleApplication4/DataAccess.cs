5fb823b1c082687f5ce7a5501160b94c8b3bdac7
Manfri <vfaermann@gmail.com>
2015-08-14 15:23:43 +0200

    1 commit 14.08.2015

---
+++ ConsoleApplication4/DataAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBDocu_ELO_Export
+{
+    class DataAccess
+    {
+        private string anlageName;
+        private string ebene2Name;
+        private string ebene3Name50;
+        private string ebene3Name60;
+        private string folderProject;
+        private string folderPrTypELOName;
+        private short folderPrTyp;
+
+       
+
+        public DataAccess()
+        {
+            using (ProjektELODataContext pr = new ProjektELODataContext())
+            {
+                string UserName = Environment.UserName;
+                int? projektNumber = pr.tblEB_ELO_Projektinfos.Where(a => a.Benutzer == UserName).Select(b => b.Projektnummer).FirstOrDefault<int?>();
+                if (projektNumber != null)
+                {
+                    var tPr = pr.tblProjektes.Where(a => a.Projektnummer == projektNumber).Select(b => b.Anlagenname).ToList<String>();
+                    anlageName = projektNumber.ToString().Length == 4 ? String.Format("00{0}", projektNumber.ToString()) : projektNumber.ToString().Length == 5 ? String.Format("0{0}", projektNumber.ToString()) : projektNumber.ToString(); 
+                    anlageName += " " + tPr[0];
+                    tPr = pr.tblProjektes.Where(a => a.Projektnummer == projektNumber).Select(b => b.Projektpfad).ToList<String>();
+                    folderProject = tPr[0];
+                    tPr = pr.tblELONamenProjekteEbene2s.Where(a => a.Ebene2NameID == 110).Select(b => b.Ebene2Name).ToList<String>();
+                    ebene2Name = String.Format("110 {0}", tPr[0]);
+                    tPr = pr.tblELONamenProjekteEbene3s.Where(a => a.Ebene2NameID == 110 & a.Ebene3NameID == 50).Select(b => b.Ebene3Name).ToList<String>();
+                    ebene3Name50 = String.Format("050 {0}", tPr[0]);
+                    tPr = pr.tblELONamenProjekteEbene3s.Where(a => a.Ebene2NameID == 110 & a.Ebene3NameID == 60).Select(b => b.Ebene3Name).ToList<String>();
+                    ebene3Name60 = String.Format("060 {0}", tPr[0]);
+                    folderPrTyp = pr.tblProjektes.Where(a => a.Projektnummer == projektNumber).Select(b => b.Projekttyp).FirstOrDefault<short>();
+                    tPr = pr.tblProjekttypens.Where(a => a.Projekttyp == folderPrTyp).Select(b => b.ELOName).ToList<String>();
+                    folderPrTypELOName = tPr[0];
+                }
+                
+            }
+        }
+
+        public string AnlageName { get { return anlageName; } }
+        public string FolderProject { get { return folderProject; } }
+        public string Ebene2Name { get { return ebene2Name; } }
+        public string Ebene3Name50 { get { return ebene3Name50; } }
+        public string Ebene3Name60 { get { return ebene3Name60; } }
+        public string FolderPrTypELOName { get { return folderPrTypELOName; } }
+        public short FolderPrTyp { get { return folderPrTyp; } }
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using EloixClient.IndexServer;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace EBDocu_ELO_Export
+{
+    class Program
+    {
+        
+        static IXConnection conn;               
+        const int findMax = 100000;
+
+        static void Main(string[] args)
+        {
+            string[] files;
+            
+            DataAccess da = new DataAccess(); 
+
+            IXConnFactory connFact = new IXConnFactory("http://SERVER-SQL:8080/ix-Projekte_und_Fibu/ix", "", "1.0");
+            conn = connFact.Create("Faermann", "Faermann", "EDV-Faermann-8", null);
+
+
+            string[] directs = getDirectory(da);
+
+            string parentId = setParentId(da); 
+
+            foreach (string direct in directs)
+            {
+                if (!isFolderDocuExist(nameFolderFile(direct), parentId, false))
+                    createFolder(nameFolderFile(direct), parentId);
+                files = Directory.GetFiles(direct);
+                foreach (string file in files)
+                {
+                    if (!isFolderDocuExist(nameFolderFile(file), parentId,true))
+                        createDocument(file, String.Format("{0}/{1}", parentId, nameFolderFile(direct)));
+                }
+            } 
+        }
+
+        static string[] getDirectory(DataAccess da)
+        {
+            string[] dir;
+            const string directAddition = @"Engineering Base\EB-Exporte\Dokumentation";
+            if (da.FolderProject.Substring(da.FolderProject.Length - 1, 1) == @"\")             
+                dir = Directory.GetDirectories(String.Format("{0}{1}", da.FolderProject, directAddition));            
+            else
+                dir = Directory.GetDirectories(String.Format("{0}\\{1}", da.FolderProject, directAddition));            
+            return dir;
+        }
+
+        static string setParentId(DataAccess da)
+        {
+            string parentId = "";
+
+            parentId = String.Format("{0}/{1}/{2}/{3}", "ARCPATH:", "Projekte", da.FolderPrTypELOName, da.AnlageName);                                                                                
+            if (!isFolderDocuExist(da.Ebene2Name, parentId, false))
+                    createFolder(da.Ebene2Name, parentId);
+            parentId = String.Format("{0}/{1}",parentId, da.Ebene2Name); 
+            if (!isFolderDocuExist(da.Ebene3Name50, parentId, false))
+                    createFolder(da.Ebene3Name50, parentId);
+            if (!isFolderDocuExist(da.Ebene3Name60, parentId, false))
+                    createFolder(da.Ebene3Name60, parentId);
+            parentId = String.Format("{0}/{1}", parentId, da.Ebene3Name50);
+           // parentId = String.Format("ARCPATH:{0}", parentId);
+            return parentId;
+        }
+
+        static bool isFolderDocuExist(string folder, string actFolder, bool isFile)
+        {
+            bool result = false;
+            FindInfo fi = new FindInfo();
+            fi.findChildren = new FindChildren();
+            fi.findChildren.parentId = actFolder;
+            fi.findChildren.endLevel = 0;
+
+            FindResult fr = conn.Ix.findFirstSords(fi, findMax, SordC.mbMin);
+            foreach (Sord sord in fr.sords)
+            {
+                if (!isFile && sord.name == folder && sord.type < SordC.LBT_DOCUMENT && 0 < sord.type)
+                    result = true;
+                if (isFile && sord.name == folder && sord.type < SordC.LBT_DOCUMENT_MAX && SordC.LBT_DOCUMENT < sord.type)
+                    result = true;
+            }
+            return result;
+        }
+
+        static void createFolder(string folder, string actFolder)
+        {
+            EditInfo ed = conn.Ix.createSord(actFolder, "", EditInfoC.mbSord);
+            ed.sord.name = folder;
+            ed.sord.id = conn.Ix.checkinSord(ed.sord, SordC.mbAll, LockC.NO);
+        }
+
+        static string nameFolderFile(string folder)
+        {
+            const String pattern = @"([.\\])";
+            String[] elements = Regex.Split(folder, pattern);
+            if (elements[elements.Length - 2] != ".")
+                return elements[elements.Length - 1];	
+            else
+                return elements[elements.Length - 3];	
+        }
+
+        static void createDocument(string file, string actFolder)
+        {
+            Document doc = new Document();
+            //Step 1
+            EditInfo ed = conn.Ix.createDoc(actFolder, "", null, EditInfoC.mbSordDocAtt);
+            Sord sord = ed.sord;
+            sord.name = nameFolderFile(file);            
+            sord.IDateIso = DateTime.Now.ToString("u");
+            //Step 2            
+            doc.docs = new DocVersion[] { new DocVersion() };
+            doc.docs[0].pathId = sord.path;
+            doc.docs[0].encryptionSet = sord.details.encryptionSet;
+            doc.docs[0].ext = conn.GetFileExt(file);
+            doc = conn.Ix.checkinDocBegin(doc);
+            // Step 3
+            doc.docs[0].uploadResult = conn.Upload(doc.docs[0].url, file);
+            // Step 4
+            doc = conn.Ix.checkinDocEnd(sord, SordC.mbAll, doc, LockC.NO);
+        }
+    }
+}
